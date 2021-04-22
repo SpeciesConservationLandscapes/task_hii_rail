6d7d6b808adc14b3582ce71d4a38bbb52d7b044f
Nathaniel <nathanielpaulrobinson@gmail.com>
2021-04-22 10:57:15 -0600

    refactored rail task with updated OSM and VMap

--- src/hii_rail.py
+++ src/hii_rail.py
@@ -5,203 +5,183 @@ from task_base import HIITask
 
 
 class HIIRail(HIITask):
-    ee_rootdir = "projects/HII/v1"
-    ee_driverdir = "driver/rail"
-    ee_hiistatic_osm = "projects/HII/v1/source/osm_earth/"
-    ee_hiistatic_infra = "projects/HII/v1/source/infra/"
-    ee_hiistatic_physical = "projects/HII/v1/source/phys/"
-    scale = 300
+    SCALE = 300
+    OSM_START = datetime(2012, 9, 12).date()
+    NOMINAL_RAIL_WIDTH = 300  # width of roads in inputs
+    DIRECT_INFLUENCE_WIDTH = 1000  # total width of direct influence (meters)
+    INDIRECT_INFLUENCE_RADIUS = (
+        15000  # distance from road indirect influence limit (meters)
+    )
     DECAY_CONSTANT = -0.0002
-    INDIRECT_INFLUENCE = 4
-
-
-    railway_abandoned_weighting = 4
-    railway_disused_weighting = 4
-    railway_funicular_weighting = 10
-    railway_halt_weighting = 10
-    railway_light_rail_weighting = 10
-    railway_miniature_weighting = 10
-    railway_monorail_weighting = 10
-    railway_narrow_gauge_weighting = 10
-    railway_platform_weighting = 10
-    railway_preserved_weighting = 10
-    railway_rail_weighting = 10
-    railway_station_weighting = 10
-    railway_subway_weighting = 10
-    railway_tram_weighting = 10
-
+    DIRECT_INDIRECT_INFLUENCE_RATIO = 0.5
 
     inputs = {
-        "railway_abandoned": {
+        "osm": {
             "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/abandoned",
+            "ee_path": "projects/HII/v1/osm/osm_image",
+            "maxage": 1,
         },
-        "railway_disused": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/disused",
-        },
-        "railway_funicular": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/funicular",
-        },
-        "railway_halt": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/halt",
-        },
-        "railway_light_rail": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/light_rail",
-        },
-        "railway_miniature": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/miniature",
-        },
-        "railway_monorail": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/monorail",
-        },
-        "railway_narrow_gauge": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/narrow_gauge",
-        },
-        "railway_platform": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/platform",
-        },
-        "railway_preserved": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/preserved",
-        },
-        "railway_rail": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/rail",
-        },
-        "railway_station": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/station",
-        },
-        "railway_subway": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/subway",
-        },
-        "railway_tram": {
-            "ee_type": HIITask.IMAGECOLLECTION,
-            "ee_path": f"{ee_hiistatic_osm}railway/tram",
-        },
-        "watermask": {
+        # TODO: refactor source dir structure
+        "vmap": {
             "ee_type": HIITask.IMAGE,
-            "ee_path": f"{ee_hiistatic_physical}watermask_jrc70_cciocean",
+            "ee_path": "projects/HII/v1/source/infra/vMap-rails-v1-global",
+            "static": True,
+        },
+        "water": {
+            "ee_type": HIITask.IMAGE,
+            "ee_path": "projects/HII/v1/source/phys/watermask_jrc70_cciocean",
             "static": True,
         },
     }
 
+    railway_weights = {
+        "railway_abandoned": 4,
+        "railway_disused": 4,
+        "railway_funicular": 10,
+        "railway_halt": 10,
+        "railway_light_rail": 10,
+        "railway_miniature": 10,
+        "railway_monorail": 10,
+        "railway_narrow_gauge": 10,
+        "railway_platform": 10,
+        "railway_preserved": 10,
+        "railway_rail": 10,
+        "railway_station": 10,
+        "railway_subway": 10,
+        "railway_tram": 10,
+        "vmap_rails": 10,
+    }
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.realm = kwargs.pop("realm", None)
-        self.set_aoi_from_ee('projects/HII/v1/source/realms/' + self.realm)  
+        self.scale = self.SCALE
+        self.osm, _ = self.get_most_recent_image(
+            ee.ImageCollection(self.inputs["osm"]["ee_path"])
+        )
+        self.vmap = ee.Image(self.inputs["vmap"]["ee_path"])
+        self.water = ee.Image(self.inputs["water"]["ee_path"])
+        self.rail_direct_cost = None
+        self.rail_indirect_cost = None
+        self.kernel = {
+            "DIRECT": ee.Kernel.euclidean(
+                radius=self.DIRECT_INFLUENCE_WIDTH / 2, units="meters"
+            ),
+            "INDIRECT": ee.Kernel.euclidean(
+                radius=self.INDIRECT_INFLUENCE_RADIUS, units="meters"
+            ),
+        }
+
+    def osm_vmap_combined_influence(self):
+        osm_band_names = self.osm.bandNames()
+        rail_weights = ee.Dictionary(self.railway_weights)
+        band_names = ee.Dictionary(
+            {
+                "all": rail_weights.keys(),
+                "osm": osm_band_names.filter(
+                    ee.Filter.inList("item", rail_weights.keys())
+                ),
+                "vmap": ["vmap_rails"],
+            }
+        )
+        direct_weights = rail_weights.toImage()
+        indirect_weights = direct_weights.multiply(self.DIRECT_INDIRECT_INFLUENCE_RATIO)
+
+        osm_rails = self.osm.select(band_names.get("osm"))
+        osm_direct = (
+            osm_rails.distance(kernel=self.kernel["DIRECT"], skipMasked=False)
+            .lte((self.DIRECT_INFLUENCE_WIDTH - self.NOMINAL_RAIL_WIDTH) / 2)
+            .multiply(direct_weights.select(band_names.get("osm")))
+        )
+
+        vmap_direct = (
+            self.vmap.distance(kernel=self.kernel["DIRECT"], skipMasked=False)
+            .lte((self.DIRECT_INFLUENCE_WIDTH - self.NOMINAL_RAIL_WIDTH) / 2)
+            .multiply(direct_weights.select(band_names.get("vmap")))
+        )
+
+        non_osm = osm_direct.reduce(ee.Reducer.max()).unmask(0).eq(0).selfMask()
+
+        vmap_fill = vmap_direct.updateMask(non_osm)
+
+        rail_direct_bands = osm_direct.addBands(vmap_fill)
+
+        self.rail_direct_cost = rail_direct_bands.reduce(ee.Reducer.max()).rename(
+            "rail_direct"
+        )
+        rail_indirect_cost_distance = rail_direct_bands.distance(
+            kernel=self.kernel["INDIRECT"], skipMasked=False
+        )
+
+        rail_indirect_cost = rail_indirect_cost_distance.select(band_names.get("all"))
+
+        self.rail_indirect_cost = (
+            rail_indirect_cost.multiply(self.DECAY_CONSTANT)
+            .exp()
+            .multiply(indirect_weights.select(band_names.get("all")))
+            .updateMask(
+                rail_indirect_cost.lte(
+                    self.INDIRECT_INFLUENCE_RADIUS - (self.DIRECT_INFLUENCE_WIDTH / 2)
+                )
+            )
+            .reduce(ee.Reducer.max())
+            .rename("rail_indirect")
+        )
+
+    def vmap_influence(self):
+        vmap_direct_weight = self.rail_weights["vmap_rails"]
+        vmap_indirect_weight = vmap_direct_weight * (
+            self.DIRECT_INDIRECT_INFLUENCE_RATIO
+        )
+
+        self.rail_direct_cost = (
+            self.vmap.distance(kernel=self.kernel["DIRECT"], skipMasked=False)
+            .lte((self.DIRECT_INFLUENCE_WIDTH - self.NOMINAL_ROAD_WIDTH) / 2)
+            .multiply(vmap_direct_weight)
+            .rename("rail_direct")
+        )
+        vmap_indirect_cost_distance = self.rail_direct_cost.distance(
+            kernel=self.kernel["INDIRECT"], skipMasked=False
+        )
+        self.rail_indirect_cost = (
+            vmap_indirect_cost_distance.multiply(self.DECAY_CONSTANT)
+            .exp()
+            .multiply(vmap_indirect_weight)
+            .updateMask(
+                vmap_indirect_cost_distance.lte(
+                    self.INDIRECT_INFLUENCE_RADIUS - (self.DIRECT_INFLUENCE_WIDTH / 2)
+                )
+            )
+            .rename("rail_indirect")
+        )
 
     def calc(self):
-        watermask = ee.Image(self.inputs["watermask"]["ee_path"])
+        if self.osm:
+            self.osm_vmap_combined_influence()
+        else:
+            self.vmap_influence()
 
-        railway_abandoned, railway_abandoned_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_abandoned"]["ee_path"])
-        )
-        
-        railway_disused, railway_disused_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_disused"]["ee_path"])
-        )
-        
-        railway_funicular, railway_funicular_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_funicular"]["ee_path"])
-        )
-        
-        railway_halt, railway_halt_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_halt"]["ee_path"])
-        )
-        
-        railway_light_rail, railway_light_rail_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_light_rail"]["ee_path"])
-        )
-        
-        railway_miniature, railway_miniature_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_miniature"]["ee_path"])
-        )
-        
-        railway_monorail, railway_monorail_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_monorail"]["ee_path"])
-        )
-        
-        railway_narrow_gauge, railway_narrow_gauge_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_narrow_gauge"]["ee_path"])
-        )
-
-        railway_platform, railway_platform_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_platform"]["ee_path"])
-        )
-
-        railway_preserved, railway_preserved_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_preserved"]["ee_path"])
-        )
-        
-        railway_rail, railway_rail_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_rail"]["ee_path"])
-        )
-        
-        railway_station, railway_station_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_station"]["ee_path"])
-        )
-        
-        railway_subway, railway_subway_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_subway"]["ee_path"])
-        )
-        
-        railway_tram, railway_tram_date = self.get_most_recent_image(
-            ee.ImageCollection(self.inputs["railway_tram"]["ee_path"])
-        )
-
-        railway_total = (railway_abandoned.multiply(self.railway_abandoned_weighting)
-            .add(railway_disused.multiply(self.railway_disused_weighting))
-            .add(railway_funicular.multiply(self.railway_funicular_weighting))
-            .add(railway_halt.multiply(self.railway_halt_weighting))
-            .add(railway_light_rail.multiply(self.railway_light_rail_weighting))
-            .add(railway_miniature.multiply(self.railway_miniature_weighting))
-            .add(railway_monorail.multiply(self.railway_monorail_weighting))
-            .add(railway_narrow_gauge.multiply(self.railway_narrow_gauge_weighting))
-            .add(railway_platform.multiply(self.railway_platform_weighting))
-            .add(railway_preserved.multiply(self.railway_preserved_weighting))
-            .add(railway_rail.multiply(self.railway_rail_weighting))
-            .add(railway_station.multiply(self.railway_station_weighting))
-            .add(railway_subway.multiply(self.railway_subway_weighting))
-            .add(railway_tram.multiply(self.railway_tram_weighting))
-        ).multiply(2)
-
-        # What should be the maximum?
-        railway_total = railway_total.where(railway_total.gt(10), 10)
-
-        rail_bool = railway_total.gt(0)
-
-        rail_indirect = rail_bool.eq(0)\
-            .cumulativeCost(rail_bool, 15000)\
-            .reproject(crs=self.crs, scale=self.scale)\
-            .multiply(self.DECAY_CONSTANT)\
-            .exp()\
-            .multiply(self.INDIRECT_INFLUENCE)\
+        rail_driver = (
+            self.rail_direct_cost.addBands(self.rail_indirect_cost)
+            .reduce(ee.Reducer.max())
             .unmask(0)
-
-        rail_total = railway_total.add(rail_indirect).updateMask(watermask)
+            .updateMask(self.water)
+            .multiply(100)
+            .int()
+            .rename("hii_railway_driver")
+        )
 
         self.export_image_ee(
-            rail_total,
-            "{}/{}".format(self.ee_driverdir, "aois/" + self.realm),
+            rail_driver,
+            f"driver/railways",
         )
 
     def check_inputs(self):
-        super().check_inputs()
+        if self.taskdate >= self.OSM_START:
+            super().check_inputs()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--realm", default='Afrotropic')
     parser.add_argument("-d", "--taskdate", default=datetime.now(timezone.utc).date())
     options = parser.parse_args()
     rail_task = HIIRail(**vars(options))
